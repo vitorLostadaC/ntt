6f5460071e198bbbe8a099f85a94a2d2274093d2
vitorLostadaC <vitorlostada@hotmail.com>
2023-07-11 13:10:39 -0300

    feat: create Header

---
+++ frontend/src/layout/Header/Header.tsx
@@ -0,0 +1,10 @@
+import logo from "../../assets/logo.png"
+import "./Header.scss"
+
+export const Header = () => {
+  return (
+    <header>
+      <img src={logo} alt="NTF data" />
+    </header>
+  )
+}
