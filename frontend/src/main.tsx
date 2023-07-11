148ea90a078581b616b1c39b23e2572b00b57362
vitorLostadaC <vitorlostada@hotmail.com>
2023-07-11 13:12:14 -0300

    feat: add Header and Footer to main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,13 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
 import "./index.scss"
+import { Header } from "./layout/Header/Header.tsx"
+import { Footer } from "./layout/Footer/Footer.tsx"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Header />
     <App />
+    <Footer />
   </React.StrictMode>
 )
